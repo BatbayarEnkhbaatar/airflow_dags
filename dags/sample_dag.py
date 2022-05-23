6af6d8ec09abd213e19d0c6a9f97673e7be61c85
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-23 18:07:32 +0900

    with new Dag file for Dejon 0.3

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 from random import randint
+
 def _choosing_best_model(ti):
     accuracies = ti.xcom_pull(task_ids=[
     'training_model_A',
@@ -16,7 +17,7 @@ def _choosing_best_model(ti):
 def _training_model(model):
     return randint(1, 10)
 
-with DAG("my_dag",start_date=datetime(2021, 1 ,1),
+with DAG("Sample_dag",start_date=datetime(2021, 1 ,1),
          schedule_interval='@hourly', catchup=False) as dag:
     training_model_tasks = [
     PythonOperator(
