0699d2a1bb63cc600710c017d35e6258cf03fe93
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-23 17:29:53 +0900

    with new Dag file for Dejon

---
+++ dags/dejon_waterMeasuring.py
@@ -0,0 +1,49 @@
+
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+# from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+import getWaterMeasuringList as wml
+
+with DAG(
+    'tutorial',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function,
+        # 'on_success_callback': some_other_function,
+        # 'on_retry_callback': another_function,
+        # 'sla_miss_callback': yet_another_function,
+        # 'trigger_rule': 'all_success'
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 5, 23),
+    catchup=False,
+    tags=['Data Pipe Line '],
+) as dag:
+
+    PythonOperator(
+        task_id=f"Dejon_dataPipeline{datetime.today()}",
+        python_callable=wml.waterMeasuring(),
+    )
+
+
