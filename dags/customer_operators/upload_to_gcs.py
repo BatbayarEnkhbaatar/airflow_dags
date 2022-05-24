e610ed7389da1df5d474ab43b460bcdc5a0da826
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-24 14:57:11 +0900

    updated .1012.

--- dags/customer_operators/upload_to_gcs.py
+++ dags/customer_operators/upload_to_gcs.py
@@ -17,7 +17,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         # blob = bucket.blob(destination_blob_name)
         # blob.upload_from_filename(f"{source_file_name}.csv")
         d_file = f"{source_file_name}.csv"
-        gcs_hook = GCSHook(gcp_conn_id=gcp_conn_id)
+        gcs_hook = GCSHook(gcp_conn_id)
         gcs_hook.upload(
             bucket_name= bucket_name,
             object_name= d_file,
