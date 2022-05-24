e47853212ba09dd9c98d9e613a8f81c8a1548ec9
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-24 14:53:44 +0900

    updated .1011.

--- dags/customer_operators/upload_to_gcs.py
+++ dags/customer_operators/upload_to_gcs.py
@@ -1,6 +1,6 @@
 from google.cloud import storage
 from datetime import date
-
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
 todays_date = date.today()
 year = todays_date.year
 month = todays_date.month
@@ -10,12 +10,19 @@ month = todays_date.month
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     try:
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(destination_blob_name)
+        gcp_conn_id = "Dejon_data_Google_Storage"
 
-        blob.upload_from_filename(source_file_name)
-
+        # storage_client = storage.Client()
+        # bucket = storage_client.bucket(bucket_name)
+        # blob = bucket.blob(destination_blob_name)
+        # blob.upload_from_filename(f"{source_file_name}.csv")
+        d_file = f"{source_file_name}.csv"
+        gcs_hook = GCSHook(gcp_conn_id=gcp_conn_id)
+        gcs_hook.upload(
+            bucket_name= bucket_name,
+            object_name= d_file,
+            filename=source_file_name
+        )
         print(
             f"File {source_file_name} uploaded to {destination_blob_name}"
         )
