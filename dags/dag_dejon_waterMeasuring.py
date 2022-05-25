885652c9c068f83b62f31d4bec1f02cd91a84109
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-25 16:17:50 +0900

    2022-5-25-30

--- dags/dag_dejon_waterMeasuring.py
+++ dags/dag_dejon_waterMeasuring.py
@@ -27,7 +27,7 @@ with DAG(
 
 ) as dag:
     ScrapingData = PythonOperator(
-    task_id="Scraping_WaterMeasuring_Data_09",
+    task_id="Scraping_WaterMeasuring_Data_10",
     python_callable = waterMeasuring,
     op_kwargs={
         "year" : todays_date.year,
