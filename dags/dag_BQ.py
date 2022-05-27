62d8b916e50305579289e57009626d84ea472222
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-27 16:33:03 +0900

    new

--- dags/dag_BQ.py
+++ dags/dag_BQ.py
@@ -32,5 +32,5 @@ with DAG("Data_from_API",
     Scraping_API = PythonOperator(
         task_id="Scraping",
         python_callable=waterMeasuring,
-        op_kwargs=Variable.get("dejon_scrapping_data", deserialize_json = True))
+        op_kwargs=Variable.get("dejon_scrapping_data", deserialize_json = True)
     )
