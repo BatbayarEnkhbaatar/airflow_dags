1f0d8e891794758dfca4ef63e64bd0422a54eab3
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-27 16:26:05 +0900

    new

--- dags/dag_BQ.py
+++ dags/dag_BQ.py
@@ -7,7 +7,6 @@ from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryExecuteQueryOperator,
     BigQueryInsertJobOperator
 )
-from airflow.utils.dates import days_ago
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "solar-idea-351402")
 DATASET_NAME = os.environ.get("GCP_BIGQUERY_DATASET_NAME", "solar-idea-351402.dejon_dataset")
@@ -33,8 +32,6 @@ with DAG("Data_from_API",
         task_id="Scraping",
         python_callable=waterMeasuring,
         op_kwargs={
-            'year': '2022',
-            'month': '03',
-            'target': '3008A40'
+            Variable.get("dejon_scrapping_data", deserialize = True)
     }
     )
