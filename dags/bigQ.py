3c597f898d904c9ecfa62d76e6ec1b67c9ebb9b4
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-05-31 16:20:01 +0900

    updated

--- dags/bigQ.py
+++ dags/bigQ.py
@@ -1,20 +1,3 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
 """
 Example Airflow DAG that shows how to use SalesforceToGcsOperator.
 """
@@ -28,12 +11,12 @@ from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryDeleteDatasetOperator,
     BigQueryInsertJobOperator,
 )
-from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator, GCSDeleteBucketOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from airflow.providers.google.cloud.transfers.salesforce_to_gcs import SalesforceToGcsOperator
 
-GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "example-project")
-GCS_BUCKET = os.environ.get("GCS_BUCKET", "airflow-salesforce-bucket")
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+
+GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "solar-idea-351402")
+GCS_BUCKET = os.environ.get("GCS_BUCKET", "dejon-data-bucket01")
 DATASET_NAME = os.environ.get("DATASET_NAME", "solar-idea-351402")
 TABLE_NAME = os.environ.get("TABLE_NAME", "WaterMeasuringList ")
 GCS_OBJ_PATH = os.environ.get("GCS_OBJ_PATH", " data202205.csv")
