4e52bd140c0d3eee6968ca79ab87710a06146d73
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2022-06-02 13:04:42 +0900

    updated on 6-2

--- dags/dag_Data_from_API2GS.py
+++ dags/dag_Data_from_API2GS.py
@@ -42,7 +42,7 @@ with DAG("WaterMeasuringList",
         bucket='dejon-data-bucket01',
         source_objects=[source_object],
         destination_project_dataset_table="dejon_dataset.WaterMeasuringList_01",
-        skip_leading_rows=1,
+        skip_leading_rows=0,
         autodetect=False,
         schema_fields=[
             {"name": "ROWNO", "type": "STRING", "mode": "NULLABLE"},
